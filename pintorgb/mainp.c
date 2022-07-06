c73656c5ef52d62815dc14313b6c74ebf59f048e
TheTaTan <101066030+TheTaTan@users.noreply.github.com>
2022-07-06 11:44:44 -0600

    Update mainp.c

--- pintorgb/mainp.c
+++ pintorgb/mainp.c
@@ -12,11 +12,14 @@ void main(){
     if (joypad() == j_DOWN) {y--;}
     if (joypad() == j_RIGHT) {x++;}
     if (joypad() == j_LEFT) {x--;}
+    if (joypad() == j_A) {plot(x,y,35);}
     //controles pero atravezados
     if (joypad() == j_UP + j_RIGHT) {x++;y++;}
     if (joypad() == j_UP + j_LEFT) {x--;y++;}
     if (joypad() == j_DOWN + j_RIGTH) {y--;x++;}
     if (joypad() == j_DOWN + J_LEFT) {y--;x--;}
+    //otras funciones
+    delay(35);
   }
 
 }
