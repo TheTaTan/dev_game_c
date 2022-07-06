6c45cdf25c8490e8e883d98c5371919acc67e4d0
TheTaTan <101066030+TheTaTan@users.noreply.github.com>
2022-07-06 11:32:12 -0600

    Update mainp.c

--- pintorgb/mainp.c
+++ pintorgb/mainp.c
@@ -7,10 +7,15 @@ unasingned char y = 0;
 void main(){
   While(1){
     //controles
-    if (joypad() == j_UP) {y++}
-    if (joypad() == j_DOWN) {y--}
-    if (joypad() == j_RIGHT) {x++}
-    if (joypad() == j_LEFT) {x--}
+    if (joypad() == j_UP) {y++;}
+    if (joypad() == j_DOWN) {y--;}
+    if (joypad() == j_RIGHT) {x++;}
+    if (joypad() == j_LEFT) {x--;}
+    //controles pero atravezados
+    if (joypad() == j_UP + j_RIGHT) {x++;y++;}
+    if (joypad() == j_UP + j_LEFT) {x--;y++;}
+    if (joypad() == j_DOWN + j_RIGTH) {y--;x++;}
+    if (joypad() == j_DOWN + J_LEFT) {y--;x--;}
   }
 
 }
