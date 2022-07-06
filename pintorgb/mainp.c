d7c31a10fcf57b4ac46dcfd5bb9b0701154380af
TheTaTan <101066030+TheTaTan@users.noreply.github.com>
2022-07-06 16:36:00 -0600

    Update mainp.c

--- pintorgb/mainp.c
+++ pintorgb/mainp.c
@@ -11,16 +11,16 @@ unsigned char y = 0;
 int main() {
   while(1) {
     //otras funciones
-    if (val == -1) {val++;}
+    if (val <= -1) {val = 0;}
     if (val == 0) {cl = WHITE;}
     if (val == 1) {cl = LTGREY;}
     if (val == 2) {cl = DKGREY;}
     if (val == 3) {cl = BLACK;}
-    if (val == 4) {val--;}
+    if (val >= 4) {val = 3;}
     
     //controles
-    if (joypad() == J_UP) {y++;}
-    if (joypad() == J_DOWN) {y--;}
+    if (joypad() == J_UP) {y--;}
+    if (joypad() == J_DOWN) {y++;}
     if (joypad() == J_RIGHT) {x++;}
     if (joypad() == J_LEFT) {x--;}
     if (joypad() == J_A) {plot(x,y,cl);}
@@ -28,10 +28,10 @@ int main() {
     if (joypad() == J_SELECT) {val--;}
     
     //controles pero atravezados
-    if (joypad() == J_UP + J_RIGHT) {x++;y++;}
-    if (joypad() == J_UP + J_LEFT) {x--;y++;}
-    if (joypad() == J_DOWN + J_RIGHT) {y--;x++;}
-    if (joypad() == J_DOWN + J_LEFT) {y--;x--;}
+    if (joypad() == J_UP + J_RIGHT) {x++;y--;}
+    if (joypad() == J_UP + J_LEFT) {x--;y--;}
+    if (joypad() == J_DOWN + J_RIGHT) {x++;y++;}
+    if (joypad() == J_DOWN + J_LEFT) {x--;y++;}
     
     //retraso de FPS
     delay(35);
