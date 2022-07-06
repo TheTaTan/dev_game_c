ea1094b3b145bb94b2e0ac71e3efb5fe509302b1
TheTaTan <101066030+TheTaTan@users.noreply.github.com>
2022-07-06 11:14:35 -0600

    Update mainp.c

--- pintorgb/mainp.c
+++ pintorgb/mainp.c
@@ -1,6 +1,16 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
+unasingned char x = 0;
+unasingned char y = 0;
+
 void main(){
-  
+  While(1){
+    //controles
+    if (joypad() == j_UP) {y++}
+    if (joypad() == j_DOWN) {y--}
+    if (joypad() == j_RIGHT) {x++}
+    if (joypad() == j_LEFT) {x--}
+  }
+
 }
