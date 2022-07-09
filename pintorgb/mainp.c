8fe111b0c1828710671e269f833cbfa674ca5992
TheTaTan <101066030+TheTaTan@users.noreply.github.com>
2022-07-09 13:51:49 -0600

    Update mainp.c

--- pintorgb/mainp.c
+++ pintorgb/mainp.c
@@ -3,52 +3,88 @@
 #include <gb/drawing.h>
 
 #include "sprites/pencils.c"
+/*
+#include "sprites/data.c"
+#include "bg/bg_d.c"
+*/
 
-unsigned char val = 3;
-unsigned char cl;
+#include "sprites/titlet.c"
+#include "bg/title.c"
+
+#define GRAPHICS_WIDTH	80
+#define GRAPHICS_HEIGHT 72
+
+char val = 2;
+char cl;
 
 unsigned char x = 0;
 unsigned char y = 0;
 
+void inputt(UBYTE nintendo){
+  switch(nintendo) {
+    //CONTROLES
+    case J_UP: y--; break;
+    case J_DOWN: y++; break;
+    case J_LEFT: x--; break;
+    case J_RIGHT: x++; break;
+    //ATRAVEZADOS
+    case J_UP + J_LEFT: y--; x--; break;
+    case J_UP + J_RIGHT: y--; x++; break;
+    case J_DOWN + J_LEFT: y++; x--; break;
+    case J_DOWN + J_RIGHT: y++; x++; break;
+    //CONTROLES CON COLOR
+    case J_UP + J_A: y--; plot( x, y, cl); break;
+    case J_DOWN + J_A: y++; plot( x, y, cl); break;
+    case J_LEFT + J_A: x--; plot( x, y, cl); break;
+    case J_RIGHT + J_A: x++; plot( x, y, cl); break;
+    //ATRAVEZADOS CON COLOR
+    case J_UP + J_LEFT + J_A: y--; x--; plot( x, y, cl); break;
+    case J_UP + J_RIGHT + J_A: y--; x++; plot( x, y, cl); break;
+    case J_DOWN + J_LEFT + J_A: y++; x--; plot( x, y, cl); break;
+    case J_DOWN + J_RIGHT + J_A: y++; x++; plot( x, y, cl); break;
+    //OTROS
+    case J_A: plot( x, y, cl); break;
+    case J_B: HIDE_SPRITES; waitpad(J_B); SHOW_SPRITES; break;
+    case J_START: val++; break;
+    case J_SELECT: val--; break;
+  }
+}
+
 int main() {
 
+  //set_bkg_data(0,11,tile_data);
+  //set_bkg_tiles(0,0,20,18,tile_bg_d);
+
+  set_bkg_data(0, 28, tile_title);
+  set_bkg_tiles(0, 0, 20, 18, tile_title_gb);
+  SHOW_BKG;
+  waitpad(J_START);
+  HIDE_BKG;
+  
   set_sprite_data(0, 4, pencils);
   set_sprite_tile(0, val);
 
   while(1) {
     //otras funciones
 
+    //SHOW_BKG;
     SHOW_SPRITES;
-
-    switch (val) {
-      case -1: val = 0; break;
-      case 0: cl = WHITE; set_sprite_tile(0, val); break;
-      case 1: cl = LTGREY; set_sprite_tile(0, val); break;
-      case 2: cl = DKGREY; set_sprite_tile(0, val);  break;
-      case 3: cl = BLACK; set_sprite_tile(0, val); break;
-      case 4: val = 3; break;
-    }
-
-    //controles
-    if (joypad() == J_UP) {y--;}
-    if (joypad() == J_DOWN) {y++;}
-    if (joypad() == J_RIGHT) {x++;}
-    if (joypad() == J_LEFT) {x--;}
-    if (joypad() == J_A) {plot(x,y,cl);}
-    if (joypad() == J_START) {val++;}
-    if (joypad() == J_SELECT) {val--;}
-    
-    //controles pero atravezados
-    if (joypad() == J_UP + J_RIGHT) {x++;y--;}
-    if (joypad() == J_UP + J_LEFT) {x--;y--;}
-    if (joypad() == J_DOWN + J_RIGHT) {x++;y++;}
-    if (joypad() == J_DOWN + J_LEFT) {x--;y++;}
+    inputt(joypad());
 
     //sprites
-    move_sprite(0, x, y);
+    move_sprite(0, x + 8, y + 9);
+    
+    switch (val) {
+      case -1: val = 0; break;
+      case 0: cl = LTGREY; set_sprite_tile(0, val); break;
+      case 1: cl = DKGREY; set_sprite_tile(0, val); break;
+      case 2: cl = BLACK; set_sprite_tile(0, val);  break;
+      case 3: cl = WHITE; set_sprite_tile(0, val); break;
+      case 4: val = 3; break;
+    }
     
     //retraso de FPS
-    delay(1000);
+    delay(65);
   }
   return(0);
 }
